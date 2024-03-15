cc2e2b7f3e7a98af1b87d70c3cc288870e710759
Srinivas Kandukuri <srinivas.kandukuri@aquasec.com>
2024-03-15 14:19:11 +0530

    refactor : optimized code and handled errors to make more standard.

--- server/handlers/handlers.go
+++ server/handlers/handlers.go
@@ -1,20 +1,14 @@
 package handlers
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 
-	"github.com/labstack/gommon/log"
-
-	"github.com/boltdbgui/modules/database/usecase"
-
-	"github.com/boltdbgui/utils"
-
 	"github.com/boltdbgui/modules/database/model"
-
+	"github.com/boltdbgui/modules/database/usecase"
+	"github.com/boltdbgui/utils"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 )
 
 func SayHello(c echo.Context) error {
@@ -22,113 +16,105 @@ func SayHello(c echo.Context) error {
 }
 
 func ListElement(c echo.Context) error {
-	all, err := io.ReadAll(c.Request().Body)
-	if err != nil {
-		return err
-	}
+	// Directly unmarshal the request body into the struct
 	var reqBody model.ListElemReqBody
-	err = json.Unmarshal(all, &reqBody)
-	if err != nil {
-		return err
+	if err := c.Bind(&reqBody); err != nil {
+		log.Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Failed to parse request body: %v", err))
 	}
-	pageSize := utils.ParseInt(c.QueryParam("page_size"))
-	pageNum := utils.ParseInt(c.QueryParam("page"))
-	searchKey := c.QueryParam("key")
-	reqBody.PageSize = pageSize
-	reqBody.Page = pageNum
-	reqBody.SearchKey = searchKey
+
+	// Extract query parameters and set them in the request body
+	reqBody.PageSize = utils.ParseInt(c.QueryParam("page_size"))
+	reqBody.Page = utils.ParseInt(c.QueryParam("page"))
+	reqBody.SearchKey = c.QueryParam("key")
+
+	// Call the use case with the request body
 	resp, err := usecase.ListElement(reqBody)
 	if err != nil {
 		log.Error(err)
 		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed Listing element: %v", err))
 	}
+
 	return c.JSON(http.StatusOK, resp)
 }
 
 func AddBucket(c echo.Context) error {
-	all, err := io.ReadAll(c.Request().Body)
-	if err != nil {
-		return err
-	}
 	var reqBody model.BucketsToAdd
-	err = json.Unmarshal(all, &reqBody)
-	if err != nil {
-		return err
+	if err := c.Bind(&reqBody); err != nil {
+		log.Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Failed to parse request body: %v", err))
 	}
-	err = usecase.AddBuckets(reqBody)
-	if err != nil {
+
+	if err := usecase.AddBuckets(reqBody); err != nil {
 		log.Error(err)
 		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed Adding bucket/s: %v", err))
 	}
+
 	return c.JSON(http.StatusOK, "Buckets added successfully")
 }
 
 func AddPairs(c echo.Context) error {
-	all, err := io.ReadAll(c.Request().Body)
-	if err != nil {
-		return err
-	}
+	// Directly unmarshal the request body into the struct
 	var reqBody model.PairsToAdd
-	err = json.Unmarshal(all, &reqBody)
-	if err != nil {
-		return err
+	if err := c.Bind(&reqBody); err != nil {
+		log.Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Failed to parse request body: %v", err))
 	}
-	err = usecase.AddPairs(reqBody)
-	if err != nil {
+
+	if err := usecase.AddPairs(reqBody); err != nil {
 		log.Error(err)
 		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed Adding pair/s: %v", err))
 	}
+
 	return c.JSON(http.StatusOK, "Pairs added successfully")
 }
 
 func DeleteElement(c echo.Context) error {
-	all, err := io.ReadAll(c.Request().Body)
-	if err != nil {
-		return err
-	}
+	// Directly unmarshal the request body into the struct
 	var reqBody model.ItemToDelete
-	err = json.Unmarshal(all, &reqBody)
-	if err != nil {
-		return err
+	if err := c.Bind(&reqBody); err != nil {
+		log.Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Failed to parse request body: %v", err))
 	}
-	err = usecase.DeleteElement(reqBody)
-	if err != nil {
+
+	// Call the use case with the request body
+	if err := usecase.DeleteElement(reqBody); err != nil {
 		log.Error(err)
 		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed Deleting element : %v", err))
 	}
+
 	return c.JSON(http.StatusOK, "Deleted successfully")
 }
+
 func RenameElement(c echo.Context) error {
-	all, err := io.ReadAll(c.Request().Body)
-	if err != nil {
-		return err
-	}
+	// Directly unmarshal the request body into the struct
 	var reqBody model.ItemToRename
-	err = json.Unmarshal(all, &reqBody)
-	if err != nil {
-		return err
+	if err := c.Bind(&reqBody); err != nil {
+		log.Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Failed to parse request body: %v", err))
 	}
-	err = usecase.RenameElement(reqBody)
-	if err != nil {
+
+	// Call the use case with the request body
+	if err := usecase.RenameElement(reqBody); err != nil {
 		log.Error(err)
 		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed renaming element : %v", err))
 	}
+
 	return c.JSON(http.StatusOK, "Renamed successfully")
 }
 func UpdatePairValue(c echo.Context) error {
-	all, err := io.ReadAll(c.Request().Body)
-	if err != nil {
-		return err
-	}
+	// Directly unmarshal the request body into the struct
 	var reqBody model.ItemToUpdate
-	err = json.Unmarshal(all, &reqBody)
-	if err != nil {
-		return err
+	if err := c.Bind(&reqBody); err != nil {
+		log.Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Failed to parse request body: %v", err))
 	}
-	err = usecase.UpdatePairValue(reqBody)
-	if err != nil {
+
+	// Call the use case with the request body
+	if err := usecase.UpdatePairValue(reqBody); err != nil {
 		log.Error(err)
 		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed updating pair value : %v", err))
 	}
+
 	return c.JSON(http.StatusOK, "Updated pair value successfully")
 }
--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,28 +1,31 @@
 package routes
 
 import (
+	"io/fs"
+	"net/http"
+
 	"github.com/boltdbgui/ui"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"io/fs"
-	"net/http"
 )
 
+// RegisterStaticRoutes registers static routes for the Echo instance.
 func RegisterStaticRoutes(e *echo.Echo) {
 	staticPages := []string{"css", "styles", "img", "js", "app", "maps", "ico", "fonts", "video", "icons"}
 
 	sub, err := fs.Sub(ui.WebContent, "dist")
 	if err != nil {
+		e.Logger.Fatalf("Failed to subtree web content: %v", err)
 		return
 	}
 
 	ac := http.FS(sub)
+	contentHandler := echo.WrapHandler(http.FileServer(ac))
 
-	var contentHandler = echo.WrapHandler(http.FileServer(ac))
-	// set the root route (serving index.html)
+	// Set the root route (serving index.html)
 	e.GET("/", contentHandler)
 
-	// configure static routes
+	// Configure static routes
 	for _, path := range staticPages {
 		e.GET("/"+path+"/*", contentHandler, middleware.Gzip())
 	}
